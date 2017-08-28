0a428e2fbd7b70fb0721f6b68205d61bb3e3b1bb
like <654534641.com>
2017-08-28 16:32:35 +0800

    Initial Commit

---
+++ TestProjectDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TestProjectDemo
+//
+//  Created by like on 2017/8/28.
+//  Copyright © 2017年 like. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ TestProjectDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TestProjectDemo
+//
+//  Created by like on 2017/8/28.
+//  Copyright © 2017年 like. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
